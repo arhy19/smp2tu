69495447d386a4abac041b69ed245cf4e502e209
arhy19 <arhy19.02@gmail.com>
2025-08-21 09:31:46 +0800

    Update usulPIP.jsx

--- src/pages/layanan/usulPIP.jsx
+++ src/pages/layanan/usulPIP.jsx
@@ -15,19 +15,28 @@ const UsulPIP = () => {
       <p className="usulpip-description">
         Silakan isi formulir berikut untuk mengusulkan Program Indonesia Pintar (PIP) Fase 1.
       </p>
+
       <div className="usulpip-iframe-wrapper">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSfmY-h4JaU6-Z93_85YmrajaCMUNUc_XvAILP19i-VdEGJpAQ/viewform?embedded=true"
           width="100%"
-          height="2493px"
+          height="100%"
           frameBorder="0"
           marginHeight="0"
           marginWidth="0"
           title="Formulir Usul PIP"
         >
-          Memuat…
+          Jika formulir tidak muncul, silakan buka langsung di{' '}
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfmY-h4JaU6-Z93_85YmrajaCMUNUc_XvAILP19i-VdEGJpAQ/viewform"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            tautan ini
+          </a>.
         </iframe>
       </div>
+
       <div className="usulpip-backlink">
         <Link to="/">← Kembali ke Beranda</Link>
       </div>
